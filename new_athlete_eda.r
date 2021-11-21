95082a2f164cfc28c096a5fbcf42e35c64e24688
Choi Seokjun <Choi.Seokjun@outlook.com>
2021-11-20 17:13:02 -0800

    new athlete data _ social wellbeing

--- new_athlete_eda.r
+++ new_athlete_eda.r
@@ -16,6 +16,7 @@ new_athlete$idv_team_athlete <- factor(new_athlete$idv_team_athlete)
 
 summary(new_athlete)
 
+# pairs(new_athlete[,-c(1,5)])
 
 plot(MHC_SF_overall ~ gender, data = new_athlete)
 plot(MHC_SF_overall ~ athlete, data = new_athlete)
@@ -30,3 +31,20 @@ plot(MHC_SF_overall ~ weeks_social_distancing, data = new_athlete)
 summary(lm(MHC_SF_overall ~ athlete + gender, data = new_athlete))
 summary(lm(MHC_SF_overall ~ athlete + gender + age_group + marital + smoking + five_fruit_and_veg + hours_sleep + shielded + weeks_social_distancing, data = new_athlete))
 
+
+
+plot(social_wellbeing ~ gender, data = new_athlete)
+plot(social_wellbeing ~ athlete, data = new_athlete)
+plot(social_wellbeing ~ age_group, data = new_athlete)
+plot(social_wellbeing ~ marital, data = new_athlete)
+plot(social_wellbeing ~ smoking, data = new_athlete)
+plot(social_wellbeing ~ five_fruit_and_veg, data = new_athlete)
+plot(social_wellbeing ~ hours_sleep, data = new_athlete)
+plot(social_wellbeing ~ shielded, data = new_athlete)
+plot(social_wellbeing ~ weeks_social_distancing, data = new_athlete)
+plot(social_wellbeing ~ num_in_lockdown_bubble, data = new_athlete)
+plot(social_wellbeing ~ country_during_lockdown, data = new_athlete)
+
+summary(lm(social_wellbeing ~ athlete + gender, data = new_athlete))
+summary(lm(social_wellbeing ~ athlete + gender + age_group + marital + smoking + five_fruit_and_veg + hours_sleep + shielded + weeks_social_distancing, data = new_athlete))
+summary(lm(social_wellbeing ~ weeks_social_distancing + num_in_lockdown_bubble + hours_sleep + gender + age_group + athlete, data = new_athlete))
